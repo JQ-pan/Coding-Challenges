9b262baa0b007c28ab9eea63591a1ebe1c844136
Jiong Qi <jiongqipan@gmail.com>
2023-11-14 01:45:46 -0500

    Finished styling

--- coalition_technologies/script.js
+++ coalition_technologies/script.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sample = document.getElementsByClassName("hero");
+    // const sample = document.getElementsById("hero");
 
-    sample.addEventListener("click", function(e) {
-        console.log(e);
-    })
+    // sample.addEventListener("click", function(e) {
+    //     console.log(e);
+    // })
+
+    
 })
+
+function dropDownMenu() {
+    const nav = document.getElementById("main-nav-list");
+    console.log(nav);
+    if (nav.className === "nav-list") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "nav-list";
+    }
+}
